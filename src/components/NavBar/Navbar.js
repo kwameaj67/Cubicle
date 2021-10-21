e1ce1f0d4600b111482282131cbf85186f60b9b1
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-21 20:31:31 +0000

    styled navbar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './navbar.css'
+import logo from '../../assets/images/cubicleLogo.svg'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="navbar_container">
+            <div className="navbar_list">
+                <div className="navbar_items">
+                    <img src={logo} alt="logo" className="logo"/>
+                    <ul>
+                        <li><a href="/">Products</a></li>
+                        <li><a href="/">Resources</a></li>
+                        <li><a href="/">Pricing</a></li>
+                    </ul>
+                </div>
+                <div className="navbar_btns">
+                    <button className="signin">Sign in</button>
+                    <button className="free">Try it for free</button>
+
+                </div>    
+            </div>
         </div>
     )
 }
