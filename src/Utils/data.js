875d59fe3c068a1d4fc42ee5360b2f6e26e375a5
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-23 17:21:28 +0000

    added data

---
+++ src/Utils/data.js
@@ -0,0 +1,51 @@
+export const comments = [
+    {
+        id:1,
+        avatar: "Ellipse10.png",
+        profileName: "Sheena Ju",
+        userName:"@SheenaSpeaks",
+        companyLogo:"../assets/images/twitter.svg",
+        comment:"When you’re trying to close - stop selling. Go into project management” @cubicle"
+    },
+    {
+        id:2,
+        avatar: "../assets/images/Ellipse11.png",
+        profileName: "Sahid Mansour",
+        userName:"@SahidMs",
+        companyLogo:"../assets/images/twitter.svg",
+        comment:"Never before have I come across a tool that is so versatile and can be used by myself on a daily basis; it's become part of my life. @cubicle makes he deal flow so easy."
+    },
+    {
+        id:3,
+        avatar: "../assets/images/Ellipse12.png",
+        profileName: "Mason Green",
+        userName:"VP, Energy @ HSBC Hong Kong",
+        companyLogo:"../assets/images/linkedin.svg",
+        comment:`It's hard to imagine life before Cubicle in a lot of ways. All the features & functionality we like about Cubicle didn't exist at the time; we were using 2-3 different VDR providers before ultimately standardizing on Cubicle as a serial acquirer.\nIt’s helped getting that data into the data room down to a science.`
+    },
+    {
+        id:4,
+        avatar: "../assets/images/Ellipse13.png",
+        profileName: "Brie Larson",
+        userName:"VP, Financial Services @ Anthill Finance",
+        companyLogo:"../assets/images/linkedin.svg",
+        comment:`Due Dilligence has come a long way since the days of phyiscal data rooms.\nAs deeals becom learger, more complex, and crossed borders, new tools are required to match the data security requirements.\nA virtual data room can be a powerful engine for deal execution.\nAllow me to introduce you to Cubicle.`
+    },
+    {
+        id:5,
+        avatar: "../assets/images/Ellipse16.png",
+        profileName: "James Frost",
+        userName:"CEO, Shippr",
+        companyLogo:"../assets/images/linkedin.svg",
+        comment:`I really believe in the business model that we've created at Shippr, we get to help people prepare for their future. Cubicle, allows us, Shippr, to focus in on our business.\nOur business is not to manage data. It's not to get data in. It's not to control the data. It's to understand that we need that data, and to make sure it's in the right place, at the right time.`
+    },
+    {
+        id:6,
+        avatar: "../assets/images/Ellipse15.png",
+        profileName: "Angel Rodriguez",
+        userName:"@AngelRodri",
+        companyLogo:"../assets/images/twitter.svg",
+        comment:"I think @cubicle’s insight is invaluable on a sale process, particularly from [an advisor] perspective when you have limited resources and you want to get the best outcome. It’s a game changer."
+    },
+    
+]
